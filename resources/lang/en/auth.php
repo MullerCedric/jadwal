c8c848d559f4feb1648514acb718d61de87f631f
Cédric Müller <muller.cedric.96@gmail.com>
2019-11-11 18:19:03 +0100

    :globe_with_meridians: Add and translate auth lang files

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -15,5 +15,21 @@ return [
 
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
+    'login' => 'Log In',
+    'email_address' => 'E-Mail Address',
+    'password' => 'Password',
+    'remember_me' => 'Remember me',
+    'no_account' => 'Don\'t have an account yet?',
+    'forgotten_password' => 'Forgot Your Password?',
+    'reset_password' => 'Reset Password',
+    'confirm_password' => 'Confirm Password',
+    'please_confirm_password' => 'Please confirm your password before continuing',
+    'send_password_reset_link' => 'Send Password Reset Link',
+    'register' => 'Register',
+    'name' => 'Name',
+    'verify_your_email' => 'Verify Your Email Address',
+    'verification_link_sent' => 'A fresh verification link has been sent to your email address',
+    'check_for_verification_link' => 'Before proceeding, please check your email for a verification link',
+    'if_email_not_received' => 'If you did not receive the email',
+    'click_to_send_another' => 'click here to request another',
 ];
---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'Ces identifiants ne correspondent pas à nos enregistrements',
+    'throttle' => 'Tentatives de connexion trop nombreuses. Veuillez essayer de nouveau dans :seconds secondes.',
+    'login' => 'Se connecter',
+    'email_address' => 'Adresse e-mail',
+    'password' => 'Mot de passe',
+    'remember_me' => 'Se souvenir de moi',
+    'no_account' => 'Pas encore de compte ?',
+    'forgotten_password' => 'Mot de passe oublié?',
+    'reset_password' => 'Réinitialiser le mot de passe',
+    'confirm_password' => 'Confirmer le mot de passe',
+    'please_confirm_password' => 'Merci de confirmer votre mot de passe avant de continuer',
+    'send_password_reset_link' => 'Envoyer un lien de réinitialisation',
+    'register' => 'S\'enregistrer',
+    'name' => 'Nom',
+    'verify_your_email' => 'Verifier votre adresse e-mail',
+    'verification_link_sent' => 'Un nouvel e-mail de vérification vous a été envoyé',
+    'check_for_verification_link' => 'Avant de continuer, merci de vérifier votre adresse e-mail',
+    'if_email_not_received' => 'Si vous n\'avez pas reçu l\'e-mail',
+    'click_to_send_another' => 'cliquez ici pour en recevoir un nouveau',
+];
